0ed81bc9ed43443ad5594b84c9f58309a3ccfd37
吴曦 <Light4mare>
2020-11-23 15:18:44 +0800

    清理中间产物

--- rompermission/src/main/java/com/rompermission/result/Permission.kt
+++ rompermission/src/main/java/com/rompermission/result/Permission.kt
@@ -6,36 +6,8 @@ import android.os.Parcelable
 import android.widget.Toast
 import androidx.annotation.StringRes
 
-class Permission(val permission: String, val message: String = "", @StringRes val messageId: Int = 0) : Parcelable {
+class Permission(val permission: String, private val message: String = "", @StringRes private val messageId: Int = 0) {
     var permitted: Boolean = false
-
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readInt()) {
-        permitted = parcel.readByte() != 0.toByte()
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(permission)
-        parcel.writeString(message)
-        parcel.writeInt(messageId)
-        parcel.writeByte(if (permitted) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Permission> {
-        override fun createFromParcel(parcel: Parcel): Permission {
-            return Permission(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Permission?> {
-            return arrayOfNulls(size)
-        }
-    }
 
     fun toastError(context: Context) {
         val tips = if (message.isNotEmpty()) message else if (messageId != 0) context.getString(messageId) else message
